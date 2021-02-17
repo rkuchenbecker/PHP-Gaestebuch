35e11bbac3f3bc25c7e48cb7ae30cbd6567e9c80
rkuchenbecker <rkuchenbecker@bbw-fi.de>
2021-02-17 15:28:09 +0100

    Changes: -completed Databank input from POST

--- index.php
+++ index.php
@@ -10,11 +10,14 @@
     <h1>Gästebuch</h1>
     <section class="eintraege">
         <?php
-        $pdo = new PDO('mysql:host=localhost;dbname=gaestebuch', 'root');
+        $pdo = new PDO('mysql:host=localhost;dbname=gaestebuch', '', '');
         $pfadDatei = "Einträge.csv";
         if(count($_POST) > 0) {
-            $statement = $pdo->prepare("INSERT INTO einträge (username, email, eintrag) VALUES (?, ?, ?");
-            $statement->execute(array($_POST['name'], $_POST['email'], $_POST['nachricht']));
+            $name = $_POST['name'];
+            $email = $_POST['email'];
+            $message = $_POST['nachricht'];
+            $statement = $pdo->prepare("INSERT INTO eintraege (username, email, eintrag) VALUES (?, ?, ?)");
+            $statement->execute(array($name, $email, $message));
             $_POST["nachricht"] = str_replace(";", ",", $_POST['nachricht']);
             $daten = $_POST;
             array_push($daten, time());
